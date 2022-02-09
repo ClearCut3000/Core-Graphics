7a589be0c3bfa00767852ac547ba806771e4cda6
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-09 21:30:11 +0300

    Ellipses and checkerboards

--- Core Graphics/ViewController.swift
+++ Core Graphics/ViewController.swift
@@ -35,6 +35,34 @@ class ViewController: UIViewController {
     imageView.image = image
   }
 
+  private func drawCircle() {
+    let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+    let image = renderer.image { context in
+      let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 5, dy: 5)
+      context.cgContext.setFillColor(UIColor.red.cgColor)
+      context.cgContext.setStrokeColor(UIColor.black.cgColor)
+      context.cgContext.setLineWidth(10)
+      context.cgContext.addEllipse(in: rectangle)
+      context.cgContext.drawPath(using: .fillStroke)
+    }
+    imageView.image = image
+  }
+
+  private func   drawCheckerboard() {
+    let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+    let image = renderer.image { context in
+      context.cgContext.setFillColor(UIColor.black.cgColor)
+      for row in 0 ..< 8 {
+        for column in 0 ..< 8 {
+          if (row+column) % 2 == 0 {
+            context.cgContext.fill(CGRect(x: column * 64, y: row * 64, width: 64, height: 64))
+          }
+        }
+      }
+    }
+    imageView.image = image
+  }
+
   //MARK: - Actions
   @IBAction func redrawTapped(_ sender: UIButton) {
     currentDrawType += 1
@@ -44,6 +72,10 @@ class ViewController: UIViewController {
     switch currentDrawType {
     case 0:
       drawRectangle()
+    case 1:
+      drawCircle()
+    case 3:
+      drawCheckerboard()
     default:
       break
     }
