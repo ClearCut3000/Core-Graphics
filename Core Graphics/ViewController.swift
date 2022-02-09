1292d2690fa117d539424763bd4f354305be32c2
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-09 23:24:05 +0300

    Images and text

--- Core Graphics/ViewController.swift
+++ Core Graphics/ViewController.swift
@@ -101,6 +101,24 @@ class ViewController: UIViewController {
     imageView.image = image
   }
 
+  private func drawImagesAndText() {
+    let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+    let image = renderer.image { context in
+      let paragraphStyle = NSMutableParagraphStyle()
+      paragraphStyle.alignment = .center
+    let attributesOfString: [NSAttributedString.Key: Any] = [
+      .font: UIFont.systemFont(ofSize: 36),
+      .paragraphStyle: paragraphStyle
+    ]
+      let string = "The best-laid schemes o'\nmice an' men gang aft agley"
+      let attributedString = NSAttributedString(string: string, attributes: attributesOfString)
+      attributedString.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, context: nil)
+      let mouse = UIImage(named: "mouse")
+      mouse?.draw(at: CGPoint(x: 300, y: 150))
+    }
+    imageView.image = image
+  }
+
   //MARK: - Actions
   @IBAction func redrawTapped(_ sender: UIButton) {
     currentDrawType += 1
@@ -118,6 +136,8 @@ class ViewController: UIViewController {
       drawRotatedSquares()
     case 4:
       drawLines()
+    case 5:
+      drawImagesAndText()
     default:
       break
     }
