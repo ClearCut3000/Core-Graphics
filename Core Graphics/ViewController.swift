c5274c02409231df678a2186ee522cfd1046ede4
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-02-09 17:16:12 +0300

    Initial commit + Creating the sandbox

--- Core Graphics/ViewController.swift
+++ Core Graphics/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  var currentDrawType = 0
+
+  //MARK: - Outlets
+  @IBOutlet var imageView: UIImageView!
+
+  //MARK: - ViewLifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    drawRectangle()
   }
 
+  //MARK: - Methods
+  private func drawRectangle() {
+    currentDrawType += 1
+    if currentDrawType > 5 {
+      currentDrawType = 0
+    }
+    switch currentDrawType {
+    case 0:
+      drawRectangle()
+    default:
+      break
+    }
+  }
+
+  //MARK: - Actions
+  @IBAction func redrawTapped(_ sender: UIButton) {
+  }
 
 }
 
